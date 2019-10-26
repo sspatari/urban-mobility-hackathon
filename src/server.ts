31e2b8dd55a07af9db7d0fcfc74a42bd32676d5b
stanislavs91 <stanislav.s@goparrot.ai>
2019-10-26 12:28:15 +0300

    Configure tslint, tsconfig, and nodemon

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
-import { Logger } from '@nestjs/common';
+import { INestApplication, Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT || 3000;
+async function bootstrap(): Promise<void> {
+  const app: INestApplication = await NestFactory.create(AppModule);
+  const port: string | number = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(`Server running on http://localhost:${port}`, 'Bootstrap');
 }
